a0002cab33fc69de5814f3b633b94d94a72481fe
nutzer15 <nutzer15@notebook15.linuxhotel.de>
2013-06-06 11:22:08 +0200

    CoffeeMaker: Add more unit tests

--- coffee_maker_unit_test.rb
+++ coffee_maker_unit_test.rb
@@ -66,4 +66,54 @@ class CoffeeMakerTest < MiniTest::Unit::TestCase
 		assert_instance_of CoffeeMaker, @machine 
 	end
 
+	def test_adding_water
+		assert_match /Water:   0\//, @machine.info
+		@machine.fill_water(1000)
+		assert_match /Water:   1000\//, @machine.info
+	end
+
+	def test_adding_coffee
+		assert_match /Coffee:  0\//, @machine.info
+		@machine.fill_coffee(100)
+		assert_match /Coffee:  100\//, @machine.info
+	end
+
+	def test_new_machine_can_make_cup
+		@machine.fill_water ( 150 )
+		@machine.fill_coffee ( 8 )
+		assert_equal "Here is your cup of coffee, enjoy.\n", @machine.make_cup
+	end
+
+	def test__water_limit
+		@machine.fill_water (149)
+		@machine.fill_coffee (8)
+		assert_raises NotEnoughWater do
+			@machine.make_cup
+		end
+	end
+
+	def test_coffee_limit
+		@machine.fill_water (150)
+		@machine.fill_coffee (7)
+		assert_raises NotEnoughCoffee do
+			@machine.make_cup
+		end
+	end
+
+	def test_machine_uses_right_amount_of_water_and_coffee
+		setup_fill_machine
+		@machine.make_cup
+		assert_match /Water:   1850\//, @machine.info
+		assert_match /Coffee:  142\//, @machine.info
+	end
+
+	def test_cleaning_machine
+		setup_fill_machine
+		5.times { @machine.make_cup }
+		assert_match /Clean in 7 cups/, @machine.info
+		assert_match /Thank you./, @machine.clean_machine
+	end
+
+  
+
 end
